488faa4ba973705b58d6623eb41e7c3e74b03cc5
aamsyahada <18523212@students.uii.ac.id>
2020-03-26 14:46:52 +0700

    commit kedua

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -6,16 +6,24 @@ public class BallScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
-	public int speed=20;
+	public int speed=30;
+
+	public Rigidbody2D sesuatu;
 
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(1,1) * speed;
+        sesuatu.velocity = new Vector2(1,1) * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+    	if(other.collider.name=="TembokKanan" || other.collider.name=="TembokKiri"){
+    		GetComponent<Transform>().position = new Vector2(0,0);
+    	}
     }
 }
