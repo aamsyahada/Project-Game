79c6df6e956b95803f0a364e17a44561402bb1b4
aamsyahada <18523212@students.uii.ac.id>
2020-04-03 16:32:05 +0700

    Animasi

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -10,15 +10,22 @@ public class BallScript : MonoBehaviour
 
 	public Rigidbody2D sesuatu;
 
+	public Animator animtr;
+
     void Start()
     {
         sesuatu.velocity = new Vector2(1,1) * speed;
+        animtr.SetBool("IsMove", true);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(sesuatu.velocity.x > 0){ //bola kearah kanan
+        	sesuatu.GetComponent<Transform>().localScale = new Vector3(1,1,1);
+        }else {
+        	sesuatu.GetComponent<Transform>().localScale = new Vector3(-1,1,1);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other) {
@@ -29,9 +36,11 @@ public class BallScript : MonoBehaviour
 
     IEnumerator jeda(){
     	sesuatu.velocity = Vector2.zero;
+    	animtr.SetBool("IsMove", false);
     	sesuatu.GetComponent<Transform>().position = new Vector2(0,0);
     	yield return new WaitForSeconds(1);
     	sesuatu.velocity = new Vector2(1,1) * speed;
+    	animtr.SetBool("IsMove", true);
     }
 
 }
