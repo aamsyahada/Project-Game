3d1a1c8bcb22b0c8e1b0b6513da4979d01a694c2
aamsyahada <18523212@students.uii.ac.id>
2020-04-16 12:20:27 +0700

    Scorring + Randomness

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
-	public int speed=30;
+	//public int speed=30;
 
 	public Rigidbody2D sesuatu;
+
+    public GameObject MasterScript;
 
 	public Animator animtr;
 
     void Start()
     {
-        sesuatu.velocity = new Vector2(1,1) * speed;
+        int x = Random.Range(0,2) * 2 - 1; //nilai x -1/1
+        int y = Random.Range(0,2) * 2 - 1; //nilai y -1/1
+        int speed = Random.Range(20,26); //nilai speed 20-25
+        sesuatu.velocity = new Vector2(x,y) * speed;
+        sesuatu.GetComponent<Transform>().position = Vector2.zero;
         animtr.SetBool("IsMove", true);
     }
 
@@ -30,6 +37,7 @@ public class BallScript : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other) {
     	if(other.collider.name=="TembokKanan" || other.collider.name=="TembokKiri"){
+            MasterScript.GetComponent<ScoringScript>().UpdateScore(other.collider.name);
     		StartCoroutine(jeda());
     	}
     }
@@ -38,9 +46,15 @@ public class BallScript : MonoBehaviour
     	sesuatu.velocity = Vector2.zero;
     	animtr.SetBool("IsMove", false);
     	sesuatu.GetComponent<Transform>().position = new Vector2(0,0);
+
     	yield return new WaitForSeconds(1);
-    	sesuatu.velocity = new Vector2(1,1) * speed;
+
+    	int x = Random.Range(0,2) * 2 - 1; //nilai x -1/1
+        int y = Random.Range(0,2) * 2 - 1; //nilai y -1/1
+        int speed = Random.Range(20,26); //nilai speed 20-25
+        sesuatu.velocity = new Vector2(x,y) * speed;
     	animtr.SetBool("IsMove", true);
+        
     }
 
 }
